ab1d5359f7a1ae006e6549307d930b554d8449cd
brivillasan <brivillayh@gmail.com>
2023-03-05 20:48:54 +0900

    commit

--- src/main/java/com/techacademy/controller/LoginController.java
+++ src/main/java/com/techacademy/controller/LoginController.java
@@ -1,8 +1,5 @@
 package com.techacademy.controller;
 
-public class LoginController {
-
-}
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
